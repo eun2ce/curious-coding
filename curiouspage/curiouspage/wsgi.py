46a226f2bdc195ce243411ad5fbf1ef5a13babdf
eheeku <eheeku@gmail.com>
2018-09-13 23:05:20 +0900

    update wsgi

--- curiouspage/curiouspage/wsgi.py
+++ curiouspage/curiouspage/wsgi.py
@@ -5,7 +5,6 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
 
 import os
 
@@ -14,3 +13,14 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "curiouspage.settings")
 
 application = get_wsgi_application()
+"""
+import os
+import sys
+
+path = os.path.expanduser('~/curiouspage')
+if path not in sys.path:
+    sys.path.append(path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'curiouspage.settings'
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
